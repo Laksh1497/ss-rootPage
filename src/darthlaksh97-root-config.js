92fcc97f56d5cf9a4431440626b305e219ea148a
Lakshmeesha Shetty <lss180005@utdallas.edu>
2021-10-05 10:26:57 -0500

    first commit

---
+++ src/darthlaksh97-root-config.js
@@ -0,0 +1,27 @@
+import { registerApplication, start } from "single-spa";
+
+registerApplication({
+  name: "@single-spa/welcome",
+  app: () =>
+    System.import(
+      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+    ),
+  activeWhen: ["/"],
+});
+
+// registerApplication({
+//   name: "@darthlaksh97/navbar",
+//   app: () => System.import("@darthlaksh97/navbar"),
+//   activeWhen: ["/"]
+// });
+
+
+registerApplication({
+  name: "@darthlaksh97/homePage",
+  app: () => System.import("@darthlaksh97/homePage"),
+  activeWhen: ["/"]
+});
+
+start({
+  urlRerouteOnly: true,
+});
